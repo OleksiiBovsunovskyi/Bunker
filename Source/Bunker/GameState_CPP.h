d0b5e27f9fafcebbfd87e4e6837af2604311d8ab
Oleksii Bovsunovskyi <>
2024-07-27 15:06:14 +0200

    INIT

---
+++ Source/Bunker/GameState_CPP.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "GameState_CPP.generated.h"
+
+/**
+ * 
+ 
+ */
+
+USTRUCT(BlueprintType)
+struct FChatMessage
+{
+	GENERATED_BODY()
+	FString Message;
+	FString Author;
+	
+};
+ 
+UCLASS()
+class BUNKER_API AGameState_CPP : public AGameState
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Replicated)
+	TArray<FChatMessage> ChatLog;
+	
+	UFUNCTION(BlueprintCallable)
+	void AddToChat(FChatMessage message);
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure);
+	FChatMessage GetChatMessages();
+	
+	
+};
