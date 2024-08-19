8941d1341e84825855670b701034eaa931ee5f35
Oleksii Bovsunovskyi <>
2024-08-19 19:06:14 +0200

    Remade chat sys, fixed player grid
    Added player data array
    Remade chat system
    Added functions for quick access of PlayerData and ChatMessages arrays, now they are automatically replicated from client to server and server to client.
    
    Added MessageData structure for storing messages in chat
    Added PlayerData structure for storing information about each player
    Both can be found in GameState_CPP.h file.

--- Source/Bunker/GameState_CPP.cpp
+++ Source/Bunker/GameState_CPP.cpp
@@ -3,3 +3,37 @@
 
 #include "GameState_CPP.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+
+void AGameState_CPP::OnRep_PlayerDataUpdate()
+{
+	OnPlayerDataChanged();
+}
+
+void AGameState_CPP::OnRep_ChatMessages()
+{
+	OnChatMessagesChanged();
+}
+
+TArray<FPlayerData> AGameState_CPP::GetPlayerData(UObject* WorldContextObject)
+{
+	return GetGameStateCPP(WorldContextObject)->PlayerDataArray;
+}
+
+TArray<FChatMessage> AGameState_CPP::GetChatLog(UObject* WorldContextObject)
+{
+	return  GetGameStateCPP(WorldContextObject)->ChatMessages;
+}
+
+AGameState_CPP* AGameState_CPP::GetGameStateCPP(UObject* WorldContextObject)
+{
+	return Cast<AGameState_CPP>( UGameplayStatics::GetGameState(WorldContextObject));
+}
+
+void AGameState_CPP::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AGameState_CPP, ChatMessages);
+	DOREPLIFETIME(AGameState_CPP, PlayerDataArray);
+}
--- Source/Bunker/GameState_CPP.h
+++ Source/Bunker/GameState_CPP.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameState.h"
+#include "Net/UnrealNetwork.h"
 #include "GameState_CPP.generated.h"
 
 /**
@@ -13,19 +14,59 @@
 
 USTRUCT(BlueprintType)
 struct FChatMessage
-{
+{	
 	GENERATED_BODY()
 public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	FString Message;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	FString Author;
 	
 };
- 
+
+
+USTRUCT(BlueprintType)
+struct FPlayerData
+{	
+	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString Name;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString Other;
+	
+};
+
 UCLASS()
 class BUNKER_API AGameState_CPP : public AGameState
 {
 	GENERATED_BODY()
 public:
-
 	
+	UPROPERTY( BlueprintReadWrite, ReplicatedUsing=OnRep_ChatMessages	)
+	TArray<FChatMessage> ChatMessages;
+
+	UPROPERTY( BlueprintReadWrite, ReplicatedUsing=OnRep_PlayerDataUpdate	)
+	TArray<FPlayerData> PlayerDataArray;
+
+	UFUNCTION()
+	void OnRep_PlayerDataUpdate();
+	UFUNCTION()
+	void OnRep_ChatMessages();
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (WorldContext = "WorldContextObject"))
+	static TArray<FPlayerData> GetPlayerData(UObject* WorldContextObject);
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (WorldContext = "WorldContextObject"))
+	static TArray<FChatMessage> GetChatLog(UObject* WorldContextObject);
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (WorldContext = "WorldContextObject"))
+	static AGameState_CPP* GetGameStateCPP(UObject* WorldContextObject);
+	
+protected:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnChatMessagesChanged();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPlayerDataChanged();
 };
