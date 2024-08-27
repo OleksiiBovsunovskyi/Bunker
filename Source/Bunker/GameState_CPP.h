6b474813f610cd2c855ac2bb2c9edbab8ce85a93
Oleksii Bovsunovskyi <>
2024-08-27 17:42:55 +0200

    SS

--- Source/Bunker/GameState_CPP.h
+++ Source/Bunker/GameState_CPP.h
@@ -35,11 +35,34 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	FString Name;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	int ID;
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	FPlayerProperty Sex;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	FPlayerProperty Job;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Age;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Health;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Hobby;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Knowledge;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Luggage;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Personality;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty Phobia;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FPlayerProperty OtherInfo;
+
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FString ID;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool IsConnected;
+
+	
+
 };
 
 UCLASS()
@@ -74,4 +97,25 @@ protected:
 	void OnChatMessagesChanged();
 	UFUNCTION(BlueprintImplementableEvent)
 	void OnPlayerDataChanged();
+
+
+	UFUNCTION(BlueprintCallable)
+	static FPlayerData GeneratePlayerData(int RandomSeed)
+	{
+		FRandomStream RandomStream(RandomSeed);
+		FPlayerData Data;
+		
+		Data.Sex = UPlayerPropertiesConfig::SexOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::SexOptions.Num() - 1)];
+		Data.Job = UPlayerPropertiesConfig::JobOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::JobOptions.Num() - 1)];
+		Data.Age = UPlayerPropertiesConfig::AgeOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::AgeOptions.Num() - 1)];
+		Data.Health = UPlayerPropertiesConfig::HealthOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::HealthOptions.Num() - 1)];
+		Data.Hobby = UPlayerPropertiesConfig::HobbyOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::HobbyOptions.Num() - 1)];
+		Data.Knowledge = UPlayerPropertiesConfig::KnowledgeOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::KnowledgeOptions.Num() - 1)];
+		Data.Luggage = UPlayerPropertiesConfig::LuggageOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::LuggageOptions.Num() - 1)];
+		Data.Personality = UPlayerPropertiesConfig::PersonalityOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::PersonalityOptions.Num() - 1)];
+		Data.Phobia = UPlayerPropertiesConfig::PhobiaOptions[RandomStream.RandRange(0, UPlayerPropertiesConfig::PhobiaOptions.Num() - 1)];
+
+		return Data;
+	}
+
 };
---
+++ Source/Bunker/PlayerController_CPP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Online/CoreOnline.h"
+#include "PlayerController_CPP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUNKER_API APlayerController_CPP : public APlayerController
+{
+	GENERATED_BODY()
+
+
+};
